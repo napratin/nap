c6927f7b6098bb1d189146453a853fffc293a985
Arpan Chakraborty <napratin@yahoo.co.in>
2013-11-26 01:47:07 -0500

    Added neuron module with neuron group generation and plotting; added simple test suite for small sets of neurons.

---
+++ nap/neuron.py
@@ -0,0 +1,298 @@
+"""A neuron model based on a biologically inspired neuronal architecture."""
+
+import logging
+from time import time
+from math import exp
+import numpy as np
+from collections import namedtuple, deque
+
+from matplotlib.pyplot import figure, plot, axis, show, subplots_adjust
+from mpl_toolkits.mplot3d import Axes3D
+
+Normal = namedtuple('Normal', ['mu', 'sigma'])  # a normal distribution, defined by mean (mu) and std. dev. (sigma)
+MultivariateNormal = namedtuple('MultivariateNormal', ['mu', 'cov'])  # a multivariate normal distribution, defined by mean vector (mu) of length N and covariance matrix (cov) of size NxN
+
+# Neuron membrane potential levels
+resting_potential = Normal(-0.07, 0.001)  # volts (mean, s.d.); resting / equillibrium potential
+threshold_potential = -0.055  # volts; level that triggers action potential
+action_potential_peak = 0.04  # volts; maximum potential reached during an action potential event
+action_potential_trough = Normal(-0.08, 0.001)  # volts; minimum potential reached due to hyperpolarization during an action potential event
+
+# Synaptic strength distribution (TODO find out from literature what these values should be)
+synaptic_strength = Normal(0.011, 0.001)  # essentially volts (mean, s.d.); potential transmitted to post-synaptic neuron when the pre-synaptic neuron undergoes an action potential event
+
+# Timing, decay and dynamic action potential parameters
+potential_decay = 1.0  # per-sec.; rate at which potential decays trying to reach equillibrium
+self_depolarization_rate = 0.75  # volts per sec.; rate at which potential rises during an action potential event
+refractory_period = 0.1  # secs.; minimum time between two action potentials (should be an emergent effect when simulating action potential in detail)
+min_update_time = 0.025  # secs.; minimum time between updates
+synapse_inhibition_period = 0.5  # secs.; duration for which the effect of inhibition lasts at a synapse
+
+# Graph parameters
+plot_colors = 'bgrcmy'
+
+
+class Projection:
+  pass
+
+
+class Dendrite(Projection):
+  pass
+
+
+class Axon(Projection):
+  pass
+
+
+class GrowthCone:
+  default_maxLength = 15.0  # maximum length a projection with these growth cone parameters can grow to
+  default_spreadFactor = 0.5  # radius at length L = spreadFactor * L
+  
+  def __init__(self, direction, maxLength=default_maxLength, spreadFactor=default_spreadFactor):
+    self.direction = direction  # unit vector
+    self.maxLength = maxLength
+    self.spreadFactor = spreadFactor
+  
+  def score(self, anchor, target):
+    """Return the probability that this growth cone starting at anchor would reach target."""
+    dist_vec = target - anchor
+    projection = np.dot(self.direction, dist_vec)
+    if projection < 0.0 or projection > self.maxLength:
+      return 0.0
+    else:
+      perp_distance = np.sqrt(np.linalg.norm(dist_vec, ord=2)**2 - projection**2)
+      perp_limit = projection * self.spreadFactor
+      return (perp_limit - perp_distance) / perp_limit if perp_distance < perp_limit else 0.0
+  
+  def __str__(self):
+    return "GrowthCone: {{ direction: {}, maxLength: {}, spreadFactor: {} }}".format(self.direction, self.maxLength, self.spreadFactor)
+
+
+class Synapse:
+  """A synapse with references to pre-synaptic and post-synaptic neurons, and an optional gatekeeper neuron."""
+  def __init__(self, pre, post, strength=None, gatekeeper=None):
+    self.pre = pre
+    self.post = post
+    self.strength = strength if strength is not None else np.random.normal(synaptic_strength.mu, synaptic_strength.sigma)
+    self.gatekeeper = gatekeeper  # NOTE do we need to store this here?
+    #print "Synapse.__init__(): pre = {}, post = {}, strength = {}, gatekeeper = {}".format(self.pre.id, self.post.id, self.strength, self.gatekeeper.id if self.gatekeeper is not None else None)
+    # TODO implement neuron-level inhibition (?), and learning/boosting of synaptic strength
+    
+    self.isInhibited = False
+    self.uninhibitAt = -1.0
+  
+  def transmitActionPotential(self, timeNow):
+    if self.isInhibited and self.uninhibitAt <= timeNow:
+      self.isInhibited = False
+    #print "Synapse.transmitActionPotential(): timeNow = {}, self.uninhibitAt = {} [{}]".format(timeNow, self.uninhibitAt, "INHIBITED" if self.isInhibited else "UNINHIBITED")
+    
+    if not self.isInhibited:
+      self.post.accumulate(self.strength)
+  
+  def inhibit(self, timeNow, duration=synapse_inhibition_period):
+    #print "Synapse.inhibit(): timeNow = {}, duration = {}".format(timeNow, duration)
+    self.isInhibited = True
+    self.uninhibitAt = timeNow + duration
+
+
+class Neuron:
+  """A simple excitable neuron cell with synaptic connections, potential accumulation and decay functionality."""
+  id_ctr = 0
+  
+  def __init__(self, location, timeNow):
+    self.id = Neuron.id_ctr
+    Neuron.id_ctr += 1
+    self.location = location  # location in 3-space
+    self.timeLastFired = self.timeLastUpdated = self.timeCurrent = timeNow
+    self.deltaTime = 0.0
+    
+    self.potential = np.random.normal(resting_potential.mu, resting_potential.sigma)  # current membrane potential
+    self.potentialAccumulated = 0.0  # potential accumulated from synaptic inputs
+    
+    self.synapses = list()
+    self.gatedSynapses = list()
+    
+    self.timeLastPlotted = self.timeCurrent  # [graph]
+    self.potentialLastPlotted = self.potential  # [graph]
+    self.plotColor = plot_colors[self.id % len(plot_colors)]  # [graph]
+  
+  def synapseWith(self, neuron, strength=None, gatekeeper=None):
+    s = Synapse(self, neuron, strength, gatekeeper)
+    self.synapses.append(s)
+    if gatekeeper is not None:
+      gatekeeper.gateSynapse(s)
+  
+  def gateSynapse(self, synapse):
+    self.gatedSynapses.append(synapse)
+  
+  def accumulate(self, deltaPotential):
+    self.potentialAccumulated += deltaPotential
+  
+  def update(self, timeNow):
+    self.timeCurrent = timeNow
+    self.deltaTime = self.timeCurrent - self.timeLastUpdated
+    if self.deltaTime < min_update_time:
+      return
+    
+    self.updatePotential()
+    self.timeLastUpdated = self.timeCurrent
+  
+  def updatePotential(self):
+    if self.potential >= action_potential_peak:
+      self.fireActionPotential()
+      self.timeLastFired = self.timeCurrent
+      self.potential = np.random.normal(action_potential_trough.mu, action_potential_trough.sigma)  # repolarization/falling phase (instantaneous)
+    
+    #self.potential = resting_potential.mu + (self.potential - resting_potential.mu) * exp(-potential_decay * self.deltaTime)  # exponential decay
+    self.potential -= potential_decay * (self.potential - resting_potential.mu) * self.deltaTime  # approximated exponential decay
+    self.potential += self.potentialAccumulated  # integrate signals accumulated from neighbors
+    self.potentialAccumulated = 0.0  # reset accumulator (don't want to double count!)
+    
+    '''
+    # Action potential - method 1: Instantaneous rise and fall
+    print self.id, self.timeCurrent, self.potential  # [log: potential]
+    if self.potential > threshold_potential and (self.timeCurrent - self.timeLastFired) >= refractory_period:
+      #self.potential = action_potential_peak  # depolarization/rising phase (instantaneous)
+      self.fireActionPotential()  # transmission
+      self.timeLastFired = self.timeCurrent
+      self.potential = np.random.normal(action_potential_trough.mu, action_potential_trough.sigma)  # repolarization/falling phase (instantaneous)
+    '''
+    
+    # Action potential - method 2: Gradual rise (more accurate, harder to do in real time)
+    if self.potential > threshold_potential and (self.timeCurrent - self.timeLastFired) >= refractory_period:
+      #print "[SELF-DEPOLARIZATION]"
+      #self.potential += self_depolarization_rate * self.deltaTime  # contant depolarization
+      self.potential += (action_potential_peak + 0.02 - self.potential) * 10 * self_depolarization_rate * self.deltaTime  # smoothed depolarization, hackish
+    
+    #print self.id, self.timeCurrent, self.potential  # [log: potential]
+    
+    # TODO implement neuron-level inhibition (?)
+  
+  def fireActionPotential(self):
+    # Transmit action potential to neighbor neurons through axon
+    #print "Neuron.fireActionPotential() [{}]".format(self.id)
+    for synapse in self.synapses:
+      synapse.transmitActionPotential(self.timeCurrent)
+      
+    for synapse in self.gatedSynapses:
+      synapse.inhibit(self.timeCurrent)
+  
+  def plot(self):
+    plot((self.timeLastPlotted, self.timeCurrent), (self.potentialLastPlotted, self.potential), self.plotColor)  # [graph]
+    self.timeLastPlotted = self.timeCurrent
+    self.potentialLastPlotted = self.potential
+
+
+class NeuronGroup:
+  default_bounds = np.float32([[-50.0, -50.0, -5.0], [50.0, 50.0, 5.0]])
+  default_distribution = MultivariateNormal(mu=np.float32([0.0, 0.0, 0.0]), cov=(np.float32([400, 400, 4]) * np.identity(3)))
+  
+  def __init__(self, numNeurons=1000, timeNow=0.0, neuronTypes=[Neuron], bounds=default_bounds, distribution=default_distribution):
+    self.numNeurons = numNeurons
+    self.timeNow = timeNow
+    self.neuronTypes = neuronTypes
+    self.bounds = bounds
+    self.distribution = distribution
+    self.isConnected = False
+    
+    self.logger = logging.getLogger(__name__)
+    self.logger.info("Creating {}".format(self))
+    self.logger.debug("Bounds: x: {}, y: {}, z: {}".format(self.bounds[:,0], self.bounds[:,1], self.bounds[:,2]))
+    #self.logger.debug("Distribution: mu: {}, cov: {}".format(self.distribution.mu, self.distribution.cov))  # ugly
+    self.neuronLocations = np.random.multivariate_normal(self.distribution.mu, self.distribution.cov, self.numNeurons)
+    #print "Neuron locations:\n", self.neuronLocations  # [debug]
+    
+    self.neurons = self.numNeurons * [None]
+    # TODO Build spatial index using oct-tree
+    self.neuronPlotColors = self.numNeurons * [None]
+    for i in xrange(self.numNeurons):
+      self.neurons[i] = Neuron(self.neuronLocations[i], self.timeNow)
+      self.neuronPlotColors[i] = self.neurons[i].plotColor
+  
+  def connectWith(self, group, maxConnectionsPerNeuron, growthCone):
+    self.growthCone = growthCone  # cache for possible display
+    self.numSynapses = 0
+    self.numDisconnectedNeurons = 0
+    
+    # * For each neuron in this group
+    for a in self.neurons:
+      # ** Find candidate neurons from the other group
+      candidates = []
+      for b in group.neurons:  # TODO: Sample/optimize
+        growthConeScore = growthCone.score(a.location, b.location)
+        if growthConeScore > 0.1:
+          candidates.append((growthConeScore, b))
+      
+      # ** Sort candidates based on scores, and pick top n (TODO: Add some probabilistic noise?)
+      candidates.sort(key=lambda pair: pair[0], reverse=True)
+      for i in xrange(min(maxConnectionsPerNeuron, len(candidates))):
+        a.synapseWith(candidates[i][1])  # TODO: Use score as synaptic strength?
+      
+      self.numSynapses += len(a.synapses)
+      if not a.synapses:
+        self.numDisconnectedNeurons += 1
+    
+    self.logger.debug("Pre: {}, post: {}, #synapses: {}, (avg.: {} per pre-neuron), #disconnected: {}".format(len(self.neurons), len(group.neurons), self.numSynapses, float(self.numSynapses) / len(self.neurons), self.numDisconnectedNeurons))
+    self.isConnected = True
+  
+  def plotNeuronLocations3D(self, ax=None, showConnections=True, groupColor=None, connectionColor=None):
+    standalone = False
+    if ax is None:
+      standalone = True
+      fig = figure()
+      ax = fig.add_subplot(111, projection='3d')
+    
+    ax.scatter(self.neuronLocations[:,0], self.neuronLocations[:,1], self.neuronLocations[:,2], c=(self.neuronPlotColors if groupColor is None else groupColor))
+    if showConnections and self.isConnected:
+      for n in self.neurons:
+        frm = n.location
+        to = n.location + self.growthCone.maxLength * self.growthCone.direction
+        #ax.plot((frm[0], to[0]), (frm[1], to[1]), (frm[2], to[2]))  # [debug: draw growth cone vector]
+        for s in n.synapses:
+          ax.plot((n.location[0], s.post.location[0]), (n.location[1], s.post.location[1]), (n.location[2], s.post.location[2]), c=(n.plotColor if connectionColor is None else connectionColor))
+    
+    if standalone:
+      plot_bounds = [min(self.bounds[0]), max(self.bounds[1])]  # so that the axes are equally scaled
+      ax.auto_scale_xyz(plot_bounds, plot_bounds, plot_bounds)
+      ax.set_xlabel("X")
+      ax.set_ylabel("Y")
+      ax.set_zlabel("Z")
+      show()
+  
+  def __str__(self):
+    return "NeuronGroup: {{ numNeurons: {}, neuronTypes: [{}] }}".format(self.numNeurons, ", ".join(t.__name__ for t in self.neuronTypes))
+  
+  def __repr__(self):
+    return "NeuronGroup: {{ numNeurons: {}, neuronTypes: [{}], bounds: {}, distribution: {} }}".format(self.numNeurons, ", ".join(t.__name__ for t in self.neuronTypes), repr(self.bounds), self.distribution)
+
+
+def plotNeuronGroups(neuronGroups, showConnections=True, groupColors=None, connectionColors=None):
+  if groupColors == None:
+    groupColors = [None] * len(neuronGroups)
+  if connectionColors == None:
+    connectionColors = [None] * len(neuronGroups)
+  fig = figure()
+  ax = fig.add_subplot(111, projection='3d')
+  plot_bounds = [np.inf, -np.inf]
+  for group, groupColor, connectionColor in zip(neuronGroups, groupColors, connectionColors):
+    group.plotNeuronLocations3D(ax, showConnections=showConnections, groupColor=groupColor, connectionColor=connectionColor)
+    plot_bounds = [min(plot_bounds[0], min(group.bounds[0])), max(plot_bounds[1], max(group.bounds[1]))]  # so that the axes are equally scaled
+  ax.auto_scale_xyz(plot_bounds, plot_bounds, plot_bounds)
+  ax.set_xlabel("X")
+  ax.set_ylabel("Y")
+  ax.set_zlabel("Z")
+  subplots_adjust(left=0.0, right=1.0, top=1.0, bottom=0.0)
+  show()
+
+
+if __name__ == "__main__":
+  logging.basicConfig(format="%(levelname)s | %(name)s | %(funcName)s() | %(message)s", level=logging.DEBUG)  # sets up basic logging, if it's not already configured
+  startTime = time()
+  timeNow = 0.0
+  group1 = NeuronGroup(numNeurons=1000, timeNow=timeNow)
+  group2 = NeuronGroup(numNeurons=500, timeNow=timeNow, bounds=np.float32([[-25.0, -25.0, 7.5], [25.0, 25.0, 12.5]]), distribution=MultivariateNormal(mu=np.float32([0.0, 0.0, 10.0]), cov=(np.float32([400, 400, 4]) * np.identity(3))))
+  growthConeDirection = group2.distribution.mu - group1.distribution.mu
+  growthConeDirection /= np.linalg.norm(growthConeDirection, ord=2)  # need a unit vector
+  group1.connectWith(group2, maxConnectionsPerNeuron=25, growthCone=GrowthCone(growthConeDirection))
+  plotNeuronGroups([group1, group2], showConnections=True, groupColors=['b', 'r'], connectionColors=[None, None])
+  # NOTE: For connectionColors, pass None to draw connection lines with pre-neuron's color; or specify colors explicitly: connectionColors=[(0.9, 0.8, 1.0, 0.5), None]
---
+++ nap/tests/test_neuron.py
@@ -0,0 +1,212 @@
+from time import time, sleep
+from unittest import TestCase
+import numpy as np
+
+from nap.neuron import Neuron, resting_potential, threshold_potential, action_potential_peak, action_potential_trough, synaptic_strength
+
+from matplotlib.pyplot import figure, plot, subplot, subplots_adjust, draw, pause, hold, show, xlim, ylim, title, xlabel, ylabel, axhline
+import matplotlib as mpl
+mpl.rc('axes', titlesize=22, labelsize=20)
+mpl.rc('xtick', labelsize=16)
+mpl.rc('ytick', labelsize=16)
+
+holdPlots = False  # wait for user to close plot windows?
+
+
+class TestNeuron(TestCase):
+  """Test neuron functions individually and in small groups."""
+  
+  def setUp(self):
+    pass
+  
+  def tearDown(self):
+    pass
+  
+  def setup_neuron_plot(self, plotTitle="Neuron", xlab="Time (s)", ylab="Membrane potential (V)"):
+    if plotTitle is not None: title(plotTitle)
+    if xlab is not None: xlabel(xlab)
+    if ylab is not None: ylabel(ylab)
+    
+    axhline(y = resting_potential.mu, color = 'k', linestyle = '--')
+    axhline(y = threshold_potential, color = 'r', linestyle = '--')
+    axhline(y = action_potential_peak, color = 'c', linestyle = '--')
+    axhline(y = action_potential_trough.mu, color = 'm', linestyle = '--')
+  
+  def test_single(self, duration=10.0, delay=0.01):
+    startTime = time()
+    timeNow = 0.0
+    n = Neuron((0.0, 0.0, 0.0), timeNow)
+    
+    # Set up plotting
+    figure(figsize = (12, 9))
+    hold(True)  # [graph]
+    xlim(0.0, duration)  # [graph]
+    ylim(action_potential_trough.mu - 0.01, action_potential_peak + 0.02)  # [graph]
+    self.setup_neuron_plot("Stimulated neuron", None, "Membrane potential (V)")
+    
+    while timeNow <= duration:
+      timeNow = time() - startTime
+      n.accumulate(np.random.normal(0.001, 0.00025))
+      n.update(timeNow)
+      n.plot()  # [graph]
+      pause(delay)  # [graph]
+      #sleep(delay)
+    
+    if holdPlots:
+      # Show plot (and wait till it's closed)
+      show()  # [graph]
+  
+  def test_pair(self, pre_duration=2.0, stimulus_duration=10.0, post_duration=2.0, delay=0.01):
+    total_duration = pre_duration + stimulus_duration + post_duration
+    stimulus_begin = pre_duration
+    stimulus_end = pre_duration + stimulus_duration
+    
+    startTime = time()
+    timeNow = 0.0
+    
+    n1 = Neuron((0.0, 0.0, 0.0), timeNow)
+    n2 = Neuron((0.0, 0.0, 1.0), timeNow)
+    n1.synapseWith(n2)
+    
+    # Set up plotting
+    figure(figsize = (12, 9))
+    hold(True)  # [graph]
+    subplot(211)  # [graph]
+    xlim(0.0, total_duration)  # [graph]
+    ylim(action_potential_trough.mu - 0.01, action_potential_peak + 0.02)  # [graph]
+    self.setup_neuron_plot("Presynaptic neuron", None, "Membrane potential (V)")
+    subplot(212)  # [graph]
+    xlim(0.0, total_duration)  # [graph]
+    ylim(action_potential_trough.mu - 0.01, action_potential_peak + 0.02)  # [graph]
+    self.setup_neuron_plot("Postsynaptic neuron", "Time (s)", "Membrane potential (V)")
+    subplots_adjust(hspace = 0.33)
+    
+    while timeNow <= total_duration:
+      timeNow = time() - startTime
+      if stimulus_begin <= timeNow <= stimulus_end:
+        n1.accumulate(np.random.normal(0.0035, 0.0005))  # TODO accumulate value based on deltaTime (since last accumulate)
+      n1.update(timeNow)
+      #print n1.id, n1.timeCurrent, n1.potential  # [log: potential]
+      n2.update(timeNow)
+      #print n2.id, n2.timeCurrent, n2.potential  # [log: potential]
+      
+      subplot(211)  # [graph]
+      n1.plot()  # [graph]
+      subplot(212)  # [graph]
+      n2.plot()  # [graph]
+      pause(delay)  # [graph]
+      #sleep(delay)
+    
+    if holdPlots:
+      show()  # [graph]
+
+
+  def test_gatekeeper(self, total_duration=14.0, stimulus_period=(2.0, 12.0), gate_period=(5.0, 8.0), delay=0.01):
+    startTime = time()
+    timeNow = 0.0
+    
+    n1 = Neuron((0.0, 0.0, 0.0), timeNow)
+    n2 = Neuron((0.0, 0.0, 1.0), timeNow)
+    g1 = Neuron((-1.0, -1.0, 1.0), timeNow)  # gatekeeper
+    n1.synapseWith(n2, None, g1)  # auto-initialize synaptic strength
+    
+    # Set up plotting
+    figure(figsize = (12, 9))
+    hold(True)  # [graph]
+    ax = subplot(311)  # [graph]
+    xlim(0.0, total_duration)  # [graph]
+    ylim(action_potential_trough.mu - 0.01, action_potential_peak + 0.01)  # [graph]
+    self.setup_neuron_plot("Presynaptic neuron", None, None)
+    ax.get_xaxis().set_ticklabels([])
+    ax = subplot(312)  # [graph]
+    xlim(0.0, total_duration)  # [graph]
+    ylim(action_potential_trough.mu - 0.01, action_potential_peak + 0.01)  # [graph]
+    self.setup_neuron_plot("Postsynaptic neuron", None, "Membrane potential (V)")
+    ax.get_xaxis().set_ticklabels([])
+    subplot(313)  # [graph]
+    xlim(0.0, total_duration)  # [graph]
+    ylim(action_potential_trough.mu - 0.01, action_potential_peak + 0.01)  # [graph]
+    self.setup_neuron_plot("Gatekeeper neuron", "Time (s)", None)
+    subplots_adjust(hspace = 0.33)
+    
+    while timeNow <= total_duration:
+      timeNow = time() - startTime
+      
+      if stimulus_period[0] <= timeNow <= stimulus_period[1]:
+        n1.accumulate(np.random.normal(0.004, 0.0005))  # TODO accumulate value based on deltaTime (since last accumulate)
+      
+      if gate_period[0] <= timeNow <= gate_period[1]:
+        g1.accumulate(np.random.normal(0.0035, 0.0005))
+      
+      n1.update(timeNow)
+      #print n1.id, n1.timeCurrent, n1.potential  # [log: potential]
+      n2.update(timeNow)
+      #print n2.id, n2.timeCurrent, n2.potential  # [log: potential]
+      g1.update(timeNow)
+      
+      subplot(311)  # [graph]
+      n1.plot()  # [graph]
+      subplot(312)  # [graph]
+      n2.plot()  # [graph]
+      subplot(313)  # [graph]
+      g1.plot()  # [graph]
+      pause(delay)  # [graph]
+      #sleep(delay)
+    
+    if holdPlots:
+      show()  # [graph]
+
+
+  def test_inhibitor(self, total_duration=14.0, stimulus_period=(2.0, 12.0), inhibition_period=(5.0, 8.0), delay=0.01):
+    startTime = time()
+    timeNow = 0.0
+    
+    n1 = Neuron((0.0, 0.0, 0.0), timeNow)
+    n2 = Neuron((0.0, 0.0, 1.0), timeNow)
+    i1 = Neuron((-1.0, -1.0, 1.0), timeNow)  # inhibitor
+    n1.synapseWith(n2)  # no synaptic gating
+    i1.synapseWith(n2, -np.random.normal(synaptic_strength.mu, synaptic_strength.sigma))  # inhibitory synapse
+    
+    # Set up plotting
+    figure(figsize = (12, 9))
+    hold(True)  # [graph]
+    subplot(311)  # [graph]
+    xlim(0.0, total_duration)  # [graph]
+    ylim(action_potential_trough.mu - 0.01, action_potential_peak + 0.01)  # [graph]
+    self.setup_neuron_plot("Neuron " + str(n1.id))
+    subplot(312)  # [graph]
+    xlim(0.0, total_duration)  # [graph]
+    ylim(action_potential_trough.mu - 0.01, action_potential_peak + 0.01)  # [graph]
+    self.setup_neuron_plot("Neuron " + str(n2.id))
+    subplot(313)  # [graph]
+    xlim(0.0, total_duration)  # [graph]
+    ylim(action_potential_trough.mu - 0.01, action_potential_peak + 0.01)  # [graph]
+    self.setup_neuron_plot("Neuron " + str(i1.id) + " (inhibitor)")
+    subplots_adjust(hspace = 0.33)
+    
+    while timeNow <= total_duration:
+      timeNow = time() - startTime
+      
+      if stimulus_period[0] <= timeNow <= stimulus_period[1]:
+        n1.accumulate(np.random.normal(0.004, 0.0005))  # TODO accumulate value based on deltaTime (since last accumulate)
+      
+      if inhibition_period[0] <= timeNow <= inhibition_period[1]:
+        i1.accumulate(np.random.normal(0.0035, 0.0005))
+      
+      n1.update(timeNow)
+      #print n1.id, n1.timeCurrent, n1.potential  # [log: potential]
+      n2.update(timeNow)
+      #print n2.id, n2.timeCurrent, n2.potential  # [log: potential]
+      i1.update(timeNow)
+      
+      subplot(311)  # [graph]
+      n1.plot()  # [graph]
+      subplot(312)  # [graph]
+      n2.plot()  # [graph]
+      subplot(313)  # [graph]
+      i1.plot()  # [graph]
+      pause(delay)  # [graph]
+      #sleep(delay)
+    
+    if holdPlots:
+      show()  # [graph]
