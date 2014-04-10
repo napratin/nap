cd57f4c0629e8bb9ee9ca308803614e7cb270740
Arpan Chakraborty <napratin@yahoo.co.in>
2014-04-10 03:58:46 -0400

    Modified analysis script to drop incorrect trials, generate properly sized plots

--- exp/visual_search/zelinsky/analysis.r
+++ exp/visual_search/zelinsky/analysis.r
@@ -3,8 +3,10 @@
 
 require(ggplot2)
 
-# Parameters
-exp_data = "data/0_2014-04-09_05-38-14.csv"  # example (TODO: accept command-line arg)
+# Parameters (TODO: accept command-line arg)
+#exp_data = "data/0_2014-04-09_12-49-03.csv"  # Q, 5
+exp_data = "data/0_2014-04-09_06-32-23.csv"  # O, 17
+plot_size = c(4, 4)
 
 # Read data
 dat = read.csv(exp_data, header=TRUE)
@@ -13,15 +15,24 @@ dat = dat[, (colnames(dat) != 'X')]  # drop extra NA col that PsychoPy adds, if 
 # Convert appropriate columns to factors
 dat$present = factor(dat$present, levels=c(1, 0), labels=c('Positive', 'Negative'))
 
+# Data cleaning
+dat = dat[dat$response.corr==1,]
+
 # Boxplot
 rtPlot = ggplot(dat, aes(present, y=response.rt, fill=present)) +
   geom_boxplot() +
+  #coord_cartesian(ylim=quantile(dat$response.rt, c(0.0001, 0.99))) +  # prevent outliers from skewing plot
   guides(fill=FALSE) +
-  xlab("Target presence") + ylab("Response Time (sec)") +
-  ggtitle(sprintf("Zelinsky search task\ntarget: %s; display size: %s; presence: %s, #trials: %d",
+  xlab("Target presence") + ylab("Reaction Time (sec)") +
+  ggtitle(sprintf("Zelinsky search task\ntarget: %s; display size: %s; #trials: %d",
             paste(unique(dat$target), collapse=", "),
             paste(unique(dat$num_stimuli), collapse=", "),
-            paste(levels(dat$present), collapse=", "),
-            nrow(dat)))
+            nrow(dat))) + 
+  # To include target presence levels: paste(levels(dat$present), collapse=", ")
+  theme(title=element_text(size=10),
+  		axis.text=element_text(size=10),
+        axis.title=element_text(size=10))
 
+dev.new(width=plot_size[1], height=plot_size[2])
+#X11()  # maybe useful for launching plots from command-line
 print(rtPlot)  # actually show plot (if run from command-line, this saves the plot to Rplots.pdf)
